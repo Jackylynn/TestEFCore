09045379879173a04196f34c5d932310c7171047
Jackylynn <174957717@qq.com>
2022-12-18 15:26:13 +0800

    添加项目文件。

---
+++ TestEFCore/MyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEFCore
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data source=.;Initial Catalog=Test;User ID=sa;Password=1234;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // 从当前程序集加载所有的IEntityTypeConfiguration
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+    }
+}
